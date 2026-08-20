904f34519be6c2aa8650e22892cd601bb792d1a1
nicim-tech <nici.makkonen@gmail.com>
2026-08-20 20:02:37 +0300

    initial commit

--- mod01/Hello.py
+++ mod01/Hello.py
@@ -1,1 +1,11 @@
-print("Hei, maailma!")
+print("Hei, Nici!")
+name = input("Mikä on nimesi? ")
+print("Terve, " + name + "!")
+import math
+# 1 radius = float(input("Anna ympyrän säde: "))
+# Kysy säde käyttäjältä ja laske ympyrän pinta-ala ja kehän pituus.
+radius = float(input("Anna ympyrän säde: "))
+area = math.pi * radius ** 2
+circumference = 2 * math.pi * radius
+print(f"Ympyrän pinta-ala on: {area}")
+print(f"Ympyrän ympärysmitta on: {circumference}")
--- readme.md
+++
@@ -1,4 +0,0 @@
-# Ohjelmisto 1 - Python harjoitukset
-**Nici Makkonen**
-## Moduuli 1
-Olen tehnyt tehtävät 1 ja 2.
