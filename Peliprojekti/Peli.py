65c03a12190fbc5ddce45d02cc19b8a57ee772c8
nicim-tech <nici.makkonen@gmail.com>
2026-09-03 15:34:47 +0300

    tehtävä

--- Peliprojekti/Peli.py
+++ Peliprojekti/Peli.py
@@ -1,28 +1,30 @@
-print("Terve, pelaaja!")
-nimi = input("Mikä on nimesi?")
-print(f"Hei {nimi} Tarkistetaan vielä ikäsi!")
-ika = int(input("Anna ikäsi: "))
-if ika < 12:
-    print("Et ole tarpeeksi vanha pelaamaan tätä peliä.")
-    exit()
-else:
-    print("Hienoa, aloitetaan!")
-     # Peli alkaa valitsemalla valikosta 1. Pelaa peliä
-valikko = input("Valitse valikosta: 1. Pelaa peliä, 2. Katso ohjeet, 3. Lopeta peli: ")
-if valikko == "1":
-    aloitus = input(f" Hei {nimi} oletko valmis pelaamaan? (kyllä/ei): ")
-    # Jos pelaaja on valmis, peli alkaa. Muuten ohjelma lopetetaan.
-    if aloitus == "kyllä":
-        print("Hienoa, peli alkaa!")
-    if aloitus == "ei":
-        print("Peli lopetetaan.")
-        exit()
-# Jos pelaaja valitsee ohjeet, ohjelma tulostaa ohjeet ja kysyy haluaako pelaaja palata päävalikkoon.
-if valikko == "2": 
-    ohjeet = print("Ohjeet pelin aloittamiseen: Tässä pelissä sinun tulee pelata ja kerätä erityisiä esineitä. Varo jokereita ja kerää pisteitä! Onnea peliin!")
-    print("haluatko palata päävalikkoon?")
-    if input("Kyllä/Ei: ") == "Kyllä":
-        valikko = input("Valitse valikosta: 1. Pelaa peliä, 2. Katso ohjeet, 3. Lopeta peli: ")
-if valikko == "3":
-    print("Kiitos pelaamisesta! Nähdään seuraavalla kerralla.")
-    exit()
+while True:
+    nimi = input("Mikä on nimesi? ")
+    ika = int(input("Anna ikäsi: "))
+    if ika < 12:
+        print("Et ole tarpeeksi vanha pelaamaan tätä peliä.")
+        break
+ # Peli alkaa valitsemalla valikosta 1. Pelaa peliä
+    print(f"hei nimi {nimi}, tervetuloa pelaamaan peliä!")
+    print(f"Olet {ika} vuotta vanha, joten voit pelata peliä.")
+    def valikko():
+        print(f"päävalikko")
+        print(f"Aloita peli: 1. Pelaa peliä")
+        print(f"Katso ohjeet: 2. Katso ohjeet")
+        print(f"Lopeta peli: 3. Kirjoita 'lopeta' lopettaaksesi pelin")
+        print ("")
+    while True:
+        print("")
+        valikko()
+        valinta = input("Valitse vaihtoehto: ")
+        if valinta == "1":
+            print("Aloitetaan peli!")
+        elif valinta == "2":
+                print("Ohjeet: Tässä pelissä sinun täytyy kerätä suurin kortti voittaaksesi ja välttää jokereita, jotka vievät sinut takaisin alkuun. Onnea peliin!")
+        elif valinta == "3":
+            print("Avasit lopeta peli.")
+        elif valinta == "lopeta":
+            print("Lopetetaan peli. Kiitos pelaamisesta!")
+            break
+            
+            
