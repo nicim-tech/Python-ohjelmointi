f16d9ce0a394dd8c150d3343b505b86939e643db
nicim-tech <nici.makkonen@gmail.com>
2026-09-03 15:49:13 +0300

    tehtävä

--- Peliprojekti/Peli.py
+++ Peliprojekti/Peli.py
@@ -1,22 +1,21 @@
-while True:
+while True :
     nimi = input("Mikä on nimesi? ")
     ika = int(input("Anna ikäsi: "))
     if ika < 12:
         print("Et ole tarpeeksi vanha pelaamaan tätä peliä.")
         break
- # Peli alkaa valitsemalla valikosta 1. Pelaa peliä
     print(f"hei nimi {nimi}, tervetuloa pelaamaan peliä!")
     print(f"Olet {ika} vuotta vanha, joten voit pelata peliä.")
     def valikko():
         print(f"päävalikko")
-        print(f"Aloita peli: 1. Pelaa peliä")
-        print(f"Katso ohjeet: 2. Katso ohjeet")
-        print(f"Lopeta peli: 3. Kirjoita 'lopeta' lopettaaksesi pelin")
+        print(f"Aloita peli painamalla 1")
+        print(f"Katso ohjeet painamalla 2")
+        print(f"Kirjoita 'lopeta' lopettaaksesi pelin")
         print ("")
-    while True:
+    while True :
         print("")
         valikko()
-        valinta = input("Valitse vaihtoehto: ")
+        valinta = input("Valitse toiminto: ")
         if valinta == "1":
             print("Aloitetaan peli!")
         elif valinta == "2":
@@ -26,5 +25,6 @@ while True:
         elif valinta == "lopeta":
             print("Lopetetaan peli. Kiitos pelaamisesta!")
             break
+        break
             
             
