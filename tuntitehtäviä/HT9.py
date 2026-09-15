b369a157f23c07a6129359dd63950cde41a4b608
nicim-tech <nici.makkonen@gmail.com>
2026-09-15 11:10:26 +0300

    HT9

--- tuntitehtäviä/HT9.py
+++ tuntitehtäviä/HT9.py
@@ -3,5 +3,14 @@ class inventaario:
     def _init_ (self, loitsulista,  sanakirja_reppu)
     self.loitsut = loitsut
     self.sanakirja_reppu = sanakirja_reppu
+    def loitsulista(self, loitsu1, loitsu2)
+    self.loitsu1 = loitsu1
+    self.loitsu2 = loitsu2
+inventaario = inventaario({}, {})
+inventaario.sanakirja_reppu = str({kruunu, kartta, miekka})
+inventaario.loitsulista = {"paranna, hyökkää, puolusta"}
+print = input("Lisää tavaroita reppuun: ")
+print = ("inventaario: ")
+print = ("loitsulista: ")
 
     
